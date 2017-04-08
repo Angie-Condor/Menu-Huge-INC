8696c3349d636ec52ee46b02ad4efed5560fa6c3
Angie Condor <angiegita1602@gmail.com>
2017-04-08 04:51:01 -0500

    js actualizado y estilos ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,43 @@ window.addEventListener("scroll", function(){
    }
    lastScrollTop = currentScroll;
 }, false);
+
+var divBox= document.getElementById("box-coders");
+var creandoDocumFragment = document.createDocumentFragment();
+var nombresCoders = ["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado",
+"Yessenia Huamán","Miriam Mendoza","Elizabeth Condori","Arantza Burga",
+"Grecia Rayme","Janine Vega","Rosario Felix","Daguiana Revoredo",
+"Jenny Velasquez","Nadia Cuadros","Michell More","Marilu Llamocca",
+"Mariel Garcia","Fiorella Cisneros","Geraldine Chauca","Yellitza Rivera",
+"Stephanie Hiyagon","Emma Tapia","Danna Franco","Flor Retamozo","Nathaly Pacheco",
+"Ericka Vidal","Katherine Ortega","Brilly Majuan","Flor Tello","Leslie Avendaño",
+"Cindy Mendoza","Annia Flores","Betsi Loayza","Aida Sulca","Milagros Gutierrez",
+"Nakarid Jave","Angie Condor","Maricarmen Rojas","Ariana Cabana","Flor Condori",
+"Mitch Rodríguez","Naomi Villanueva","Mari Castillo","Miriam Peralta","Karin Alejo",
+"Liliana Peña","Ruth Salvador","Maribel Sevilla","Wendy Reyes","Cinthia Avendaño",
+"Maria Grecia Cutipa","Ana Durand","Glisse Jorge","Neiza Nuñez","Sandra Solorzano"];
+
+window.onload = function() {
+    for (var i = 1; i < nombresCoders.length ; i++) {
+      var span = document.createElement("span");
+      var figCaption = document.createElement("figcaption");
+      var img = document.createElement("img");
+      var figure = document.createElement("figure");
+      var divCadaCoder = document.createElement("div");
+      var nombreCoder = document.createTextNode(nombresCoders[i-1]);
+      img.setAttribute("src","assets/img/students/"+i+".png");
+      img.setAttribute("class","foto-coders");
+      figCaption.setAttribute("class","center");
+      figure.setAttribute("class","coders");
+      divCadaCoder.setAttribute("class","cada-coder");
+
+      span.appendChild(nombreCoder);
+      figCaption.appendChild(span);
+      figure.appendChild(img);
+      figure.appendChild(figCaption);
+      divCadaCoder.appendChild(figure);
+      creandoDocumFragment.appendChild(divCadaCoder);
+      divBox.appendChild(creandoDocumFragment);
+    }
+
+}
